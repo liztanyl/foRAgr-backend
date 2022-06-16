e8564d201580c18509fcceb8920849cd3083de41
gcskhor <gcskhor@yahoo.com.sg>
2022-06-16 19:03:24 +0800

    added reviewItems controller function

--- controllers/foodItems.mjs
+++ controllers/foodItems.mjs
@@ -1,17 +1,36 @@
-export default function initFoodItemsController(db) {
-  const index = async (request, response) => {
-    try {
-      const foodItems = await db.FoodItem.findAll({
-        attributes: ['id', 'name']
-      });
-      response.send(foodItems);
-    } catch (error) {
-      console.log(error)
-      response.status(500).send();
-    }
-  };
+import { Sequelize } from 'sequelize';
 
-  return {
-    index,
-  };
+export default function initFoodItemsController(db) {
+	const index = async (request, response) => {
+		try {
+			const foodItems = await db.FoodItem.findAll({
+				attributes: ['id', 'name'],
+			});
+			response.send(foodItems);
+		} catch (error) {
+			console.log(error);
+			response.status(500).send();
+		}
+	};
+
+	const reviewItems = async (request, response) => {
+		try {
+			let { reviewItemIds } = request.params;
+			reviewItemIds = reviewItemIds.split(',');
+			const foodItems = await db.FoodItem.findAll({
+				where: {
+					id: reviewItemIds,
+				},
+			});
+			response.send(foodItems);
+		} catch (error) {
+			console.log(error);
+			response.status(500).send();
+		}
+	};
+
+	return {
+		index,
+		reviewItems,
+	};
 }
