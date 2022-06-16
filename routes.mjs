0baba21ca6f201a453d4dcf58e6d2ae70dbfb23e
cheenaeng <cheena94sing@gmail.com>
2022-06-16 19:26:40 +0800

    added post request for sending image to backend

--- routes.mjs
+++ routes.mjs
@@ -2,35 +2,44 @@ import db from './models/index.mjs';
 import initFoodItemsController from './controllers/foodItems.mjs';
 
 export default function bindRoutes(app) {
-	const FoodItemsController = initFoodItemsController(db);
+  const FoodItemsController = initFoodItemsController(db);
 
-	app.get('/', (request, response) => {
-		response.sendFile(resolve('dist', 'main.html'));
-	});
-	app.get('/item', async (req, res) => {
-		try {
-			const item = await db.FoodItem.findAll({
-				where: { name: 'beef stock' },
-				include: {
-					model: db.Category,
-					include: db.Storage,
-				},
-			});
-			res.send(item);
-		} catch (error) {
-			console.log(error);
-			res.status(404).send('Something went wrong');
-		}
-	});
-	app.get('/shelfLifeItems', async (req, res) => {
-		try {
-			const items = await db.ShelfLifeItem.findAll();
-			res.send(items);
-		} catch (error) {
-			console.log(error);
-			res.status(404).send('Something went wrong');
-		}
-	});
+  app.get('/', (request, response) => {
+    response.sendFile(resolve('dist', 'main.html'));
+  });
+  app.get('/item', async (req, res) => {
+    try {
+      const item = await db.FoodItem.findAll({
+        where: { name: 'beef stock' },
+        include: {
+          model: db.Category,
+          include: db.Storage,
+        },
+      });
+      res.send(item);
+    } catch (error) {
+      console.log(error);
+      res.status(404).send('Something went wrong');
+    }
+  });
+  app.get('/shelfLifeItems', async (req, res) => {
+    try {
+      const items = await db.ShelfLifeItem.findAll();
+      res.send(items);
+    } catch (error) {
+      console.log(error);
+      res.status(404).send('Something went wrong');
+    }
+  });
 
-	app.get('/foodItems/index', FoodItemsController.index);
+  app.get('/foodItems/index', FoodItemsController.index);
+
+  app.post('/photoData', async (req, res) => {
+    try {
+      console.log(req.body);
+    } catch (error) {
+      console.log(error);
+      res.status(404).send('Something went wrong');
+    }
+  });
 }
