724c95dcc23022ece54a825428bd0f525d14f7a8
cheenaeng <cheena94sing@gmail.com>
2022-06-18 23:58:53 +0800

    added data id

--- controllers/photoData.mjs
+++ controllers/photoData.mjs
@@ -8,7 +8,21 @@ export default function initPhotoDataController(db) {
       console.log(imageUri);
       const matchedData = getMatches();
       console.log(matchedData);
-      response.send(matchedData);
+
+      const foodItems = await db.FoodItem.findAll({
+        attributes: ['id', 'name'],
+      });
+
+      const compiledMatches = matchedData.map((data) => {
+        foodItems.forEach((food) => {
+          if (food.name === data.match[0].itemName) {
+            data.dataId = food.id;
+          }
+        });
+        return data;
+      });
+
+      response.send(compiledMatches);
     } catch (error) {
       console.log(error);
       response.status(500).send();
