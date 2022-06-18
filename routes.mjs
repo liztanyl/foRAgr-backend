17e4cbf876ad12ce70ce2b91cbbd3b95f66ac637
liztanyl <elizabeth.tanyulin@gmail.com>
2022-06-18 12:55:31 +0800

    remove sample routes

--- routes.mjs
+++ routes.mjs
@@ -1,4 +1,3 @@
-import db from './models/index.mjs';
 import initFoodItemsController from './controllers/foodItems.mjs';
 import initPhotoDataController from './controllers/photoData.mjs';
 
@@ -8,32 +7,6 @@ export default function bindRoutes(app) {
 
 	app.get('/', (request, response) => {
 		response.sendFile(resolve('dist', 'main.html'));
-	});
-	app.get('/item', async (req, res) => {
-		try {
-			const item = await db.FoodItem.findAll({
-				where: { name: 'beef stock' },
-				include: {
-					model: db.Category,
-					include: db.Storage,
-				},
-			});
-			res.send(item);
-		} catch (error) {
-			console.log(error);
-			res.status(404).send('Something went wrong');
-		}
-	});
-
-	app.get('/shelfLifeItems', async (req, res) => {
-		try {
-			const items = await db.ShelfLifeItem.findAll();
-			console.log(items);
-			res.send(items);
-		} catch (error) {
-			console.log(error);
-			res.status(404).send('Something went wrong');
-		}
 	});
 
 	app.get('/foodItems/index', FoodItemsController.index);
