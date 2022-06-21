11c1b51a00238f710a991613d26f498da1c49192
gcskhor <gcskhor@yahoo.com.sg>
2022-06-21 13:19:17 +0800

    fixed error

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -27,33 +27,14 @@ function getGoogleAuthURLHelper() {
 }
 
 export default function initUserController(db) {
-    try {
-      const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
-      const options = {
-        redirect_uri: `${FRONTEND_URL}/auth/google`,
-        client_id: process.env.OAUTH_CLIENT_ID,
-        access_type: 'offline',
-        response_type: 'code',
-        prompt: 'consent',
-        scope: [
-          'https://www.googleapis.com/auth/userinfo.profile',
-          'https://www.googleapis.com/auth/userinfo.email',
-        ].join(' '),
-      };
-
-      console.log(process.env.OAUTH_CLIENT_ID);
-
-      response.send(`${rootUrl}?${querystring.stringify(options)}`);
-    } catch (error) {
-      console.log(error);
-      response.status(500).send();
-    }
-  }; 
-
   const getGoogleAuthUrl = async (req, res) => {
-    const url = getGoogleAuthURLHelper();
-    console.log(url);
-    res.send(url);
+    try {
+      const url = getGoogleAuthURLHelper();
+      console.log(url);
+      res.send(url);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return {
