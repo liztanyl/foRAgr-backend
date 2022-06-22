79b57983e9742ad2642c31a49d9c6420801ac6e7
gcskhor <gcskhor@yahoo.com.sg>
2022-06-22 15:38:24 +0800

    updated seeders, migrations and models for updated user info

--- seeders/20220615121126-seed-data.js
+++ seeders/20220615121126-seed-data.js
@@ -5,45 +5,44 @@ const storage = require('./data/storage');
 const shelf_life_items = require('./data/shelf_life_items');
 
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		food_items.forEach((item) => {
-			item.created_at = new Date();
-			item.updated_at = new Date();
-		});
-		categories.forEach((item) => {
-			item.created_at = new Date();
-			item.updated_at = new Date();
-		});
-		storage.forEach((item) => {
-			item.created_at = new Date();
-			item.updated_at = new Date();
-		});
-		shelf_life_items.forEach((item) => {
-			item.created_at = new Date();
-			item.updated_at = new Date();
-		});
+  async up(queryInterface, Sequelize) {
+    food_items.forEach((item) => {
+      item.created_at = new Date();
+      item.updated_at = new Date();
+    });
+    categories.forEach((item) => {
+      item.created_at = new Date();
+      item.updated_at = new Date();
+    });
+    storage.forEach((item) => {
+      item.created_at = new Date();
+      item.updated_at = new Date();
+    });
+    shelf_life_items.forEach((item) => {
+      item.created_at = new Date();
+      item.updated_at = new Date();
+    });
 
-		await queryInterface.bulkInsert('users', [
-			{
-				id: 1,
-				email: 'a@a.com',
-				password: 'a',
-				name: 'User A',
-				created_at: new Date(),
-				updated_at: new Date(),
-			},
-		]);
-		await queryInterface.bulkInsert('food_items', food_items);
-		await queryInterface.bulkInsert('categories', categories);
-		await queryInterface.bulkInsert('storage', storage);
-		await queryInterface.bulkInsert('shelf_life_items', shelf_life_items);
-	},
+    await queryInterface.bulkInsert('users', [
+      {
+        id: 1,
+        email: 'a@a.com',
+        name: 'User A',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+    await queryInterface.bulkInsert('food_items', food_items);
+    await queryInterface.bulkInsert('categories', categories);
+    await queryInterface.bulkInsert('storage', storage);
+    await queryInterface.bulkInsert('shelf_life_items', shelf_life_items);
+  },
 
-	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete('shelf_life_items', null, {});
-		await queryInterface.bulkDelete('food_items', null, {});
-		await queryInterface.bulkDelete('categories', null, {});
-		await queryInterface.bulkDelete('storage', null, {});
-		await queryInterface.bulkDelete('users', null, {});
-	},
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('shelf_life_items', null, {});
+    await queryInterface.bulkDelete('food_items', null, {});
+    await queryInterface.bulkDelete('categories', null, {});
+    await queryInterface.bulkDelete('storage', null, {});
+    await queryInterface.bulkDelete('users', null, {});
+  },
 };
