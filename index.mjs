7dd835a06b45ee6446aace99dac11bc440619e5f
gcskhor <gcskhor@yahoo.com.sg>
2022-06-22 15:35:53 +0800

    Added custom user verification middleware

--- index.mjs
+++ index.mjs
@@ -3,6 +3,7 @@ import cors from 'cors';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
 import bindRoutes from './routes.mjs';
+import { verifyLoggedInUser } from './controllers/users.mjs';
 
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:19006';
 
@@ -10,14 +11,17 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: FRONTEND_URL,
-  }),
+    origin: [FRONTEND_URL, '127.0.0.1:19006'],
+  })
 );
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.json());
+
+// CUSTOM MIDDLEWARE
+app.use(verifyLoggedInUser);
 
 bindRoutes(app);
 
