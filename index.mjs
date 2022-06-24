7bc88a501dbb0046da1f2d7d38f97f654f8b80cc
gcskhor <gcskhor@yahoo.com.sg>
2022-06-24 17:54:13 +0800

    removed user verification middleware

--- index.mjs
+++ index.mjs
@@ -3,25 +3,30 @@ import cors from 'cors';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
 import bindRoutes from './routes.mjs';
-import { verifyLoggedInUser } from './controllers/users.mjs';
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:19006';
+const FRONTEND_URL =
+  process.env.FRONTEND_URL ||
+  'http://192.168.18.17:19006' ||
+  'exp://ds-v9m.gcskhor.project6-frontend.exp.direct:80';
 
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [FRONTEND_URL, '127.0.0.1:19006'],
-  }),
+    origin: [
+      FRONTEND_URL,
+      'http://localhost:19006',
+      'http://127.0.0.1:19006',
+      'http://192.168.18.17:19006',
+    ],
+  })
 );
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.json());
-
-// // CUSTOM MIDDLEWARE
-// app.use(verifyLoggedInUser);
 
 bindRoutes(app);
 
