2eb53f0097a4d620d43b0b8d3bda0198871c2481
gcskhor <gcskhor@yahoo.com.sg>
2022-06-24 18:12:21 +0800

    corrected frontend urls

--- index.mjs
+++ index.mjs
@@ -4,21 +4,13 @@ import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
 import bindRoutes from './routes.mjs';
 
-const FRONTEND_URL =
-  process.env.FRONTEND_URL ||
-  'http://192.168.18.17:19006' ||
-  'exp://ds-v9m.gcskhor.project6-frontend.exp.direct:80';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://192.168.18.17:19006';
 
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [
-      FRONTEND_URL,
-      'http://localhost:19006',
-      'http://127.0.0.1:19006',
-      'http://192.168.18.17:19006',
-    ],
+    origin: [FRONTEND_URL, 'http://localhost:19006', 'http://127.0.0.1:19006'],
   })
 );
 
